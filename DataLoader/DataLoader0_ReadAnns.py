236da1ec864251dd5aa0696a61d52dddcbd03a33
Elliot <hylee101001@gmail.com>
2019-09-18 13:02:02 -0700

    aaA

--- DataLoader/DataLoader0_ReadAnns.py
+++ DataLoader/DataLoader0_ReadAnns.py
@@ -34,29 +34,36 @@ class DataLoader0_ReadAnns():
         return img
 
     def getBBoxCenter(self, bboxes):
-        x, y = bboxes[:,0,None]+bboxes[:,2,None]/2, bboxes[:,1,None]+bboxes[:,3,None]/2
-        offset = np.zeros((x.shape[0], 2))
-        for index in range(x.shape[0]):
-            for i in range(0, self.numGridX):
-                low, high = i * self.gridW, (i + 1) * self.gridW
-                if (x[index] >= low and x[index] < high):
-                    offset[index, 0] = low
-                    break
-            for j in range(0, self.numGridY):
-                low, high = j * self.gridH, (j + 1) * self.gridH
-                if (y[index] >= low and y[index] < high):
-                    offset[index, 1] = low
-                    break
 
-        relX = x - offset[:,0, None]
-        relY = y - offset[:,1, None]
+        if bboxes.shape[0] > 0:
+            x, y = bboxes[:,0,None]+bboxes[:,2,None]/2, bboxes[:,1,None]+bboxes[:,3,None]/2
+            offset = np.zeros((x.shape[0], 2))
+            for index in range(x.shape[0]):
+                for i in range(0, self.numGridX):
+                    low, high = i * self.gridW, (i + 1) * self.gridW
+                    if (x[index] >= low and x[index] < high):
+                        offset[index, 0] = low
+                        break
+                for j in range(0, self.numGridY):
+                    low, high = j * self.gridH, (j + 1) * self.gridH
+                    if (y[index] >= low and y[index] < high):
+                        offset[index, 1] = low
+                        break
+
+            relX = x - offset[:, 0, None]
+            relY = y - offset[:, 1, None]
+        else:
+            offset = np.zeros((1, 2))
+            relX  = np.zeros((1,1))
+            relY = np.zeros((1,1))
         return offset, relX, relY
 
     def resize(self, img, bboxes, w=448, h=448):
         origW, origH = img.shape[1], img.shape[0]
         img = cv2.resize(img, (w, h))
-        bboxes[:, [0, 2]] = bboxes[:, [0, 2]] * w / origW
-        bboxes[:, [1, 3]] = bboxes[:, [1, 3]] * h / origH
+        if bboxes.shape[0] > 0:
+            bboxes[:, [0, 2]] = bboxes[:, [0, 2]] * w / origW
+            bboxes[:, [1, 3]] = bboxes[:, [1, 3]] * h / origH
         return img, bboxes
 
     def printAnnsAt(self, i):
--- DataLoader/DataLoader1_ReadAll.py
+++ DataLoader/DataLoader1_ReadAll.py
@@ -1,15 +1,27 @@
-from DataLoader.DataLoader0_ReadAnns import DataLoader0_ReadAnns
+from DataLoader.DataLoader0_ReadAnns import *
 
 class DataLoader1_ReadAll():
     def __init__(self):
-        anns = DataLoader0_ReadAnns()
-        i = 150
-        img = anns.getImgAt(i)
-        bboxes = anns.getBBoxesAt(i)
-        objNames = anns.getObjNamesAt(i)
-        objIds = anns.getObjIdsAt(i)
-        img, bboxes = anns.resize(img, bboxes)
-        offset, relX, relY = anns.getBBoxCenter(bboxes)
-        img = anns.drawBBox(img, bboxes, objNames)
-        img = anns.drawBBoxCenter(img, offset, relX, relY, bboxes.shape[0])
-        anns.showImg(img)
+        self.anns = DataLoader0_ReadAnns()
+        self.getDataLable()
+        # i = 150
+        # img, bboxes, objNames, objIds = self.getResizedInfoAt(i)
+        # offset, relX, relY = self.anns.getBBoxCenter(bboxes)
+        # img = self.anns.drawBBoxCenter(img, offset, relX, relY, bboxes.shape[0])
+        # img = self.anns.drawBBox(img, bboxes, objNames)
+        # self.anns.showImg(img)
+
+    def getDataLable(self):
+        for i in range(0, self.anns.N):
+            img, bboxes, objNames, objIds = self.getResizedInfoAt(i)
+            offset, relX, relY = self.anns.getBBoxCenter(bboxes)
+            if np.mod(i, 100) == 0:
+                print(i)
+
+    def getResizedInfoAt(self, i):
+        img = self.anns.getImgAt(i)
+        bboxes =self. anns.getBBoxesAt(i)
+        objNames = self.anns.getObjNamesAt(i)
+        objIds = self.anns.getObjIdsAt(i)
+        img, bboxes = self.anns.resize(img, bboxes)
+        return img, bboxes, objNames, objIds
