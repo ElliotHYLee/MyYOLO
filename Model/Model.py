6c0b5ab03b9d035ddedb2a2ae04002b7b017be97
Elliot <hylee101001@gmail.com>
2019-09-24 16:17:30 -0700

    aaa

--- Model/Model.py
+++ Model/Model.py
@@ -1,7 +1,7 @@
 from MyPyTorchAPI.CNNUtils import *
 import numpy as np
 from MyPyTorchAPI.CustomActivation import *
-
+import torch.nn.functional as F
 class Model_CNN_0(nn.Module):
     def __init__(self, dsName='airsim'):
         super().__init__()
@@ -14,38 +14,34 @@ class Model_CNN_0(nn.Module):
             Conv2DBlock(32, 64, kernel=3, stride=1, padding = 0, atvn='prlu', bn = True, dropout=True),
             MaxPool2DBlock(64, kernel=2, stride=2, padding= 0),
 
-            Conv2DBlock(64, 128, kernel=1, stride=1, padding=0, atvn='prlu', bn = True, dropout=True),
-            Conv2DBlock(128, 64, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
-            Conv2DBlock(64, 128, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
+            Conv2DBlock(64, 128, kernel=1, stride=1, padding=0, atvn='prlu', bn = True, dropout=False),
+            Conv2DBlock(128, 64, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
+            Conv2DBlock(64, 128, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
             MaxPool2DBlock(128, kernel=2, stride=2, padding=0),
 
-            Conv2DBlock(128, 256, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
-            Conv2DBlock(256, 128, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
-            Conv2DBlock(128, 256, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
+            Conv2DBlock(128, 256, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
+            Conv2DBlock(256, 128, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
+            Conv2DBlock(128, 256, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
             MaxPool2DBlock(256, kernel=2, stride=2, padding=0),
 
-            Conv2DBlock(256, 512, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
-            Conv2DBlock(512, 256, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
-            Conv2DBlock(256, 512, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
-            Conv2DBlock(512, 256, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
-            Conv2DBlock(256, 512, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
+            Conv2DBlock(256, 512, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
+            Conv2DBlock(512, 256, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
+            Conv2DBlock(256, 512, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
+            Conv2DBlock(512, 256, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
+            Conv2DBlock(256, 512, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
             MaxPool2DBlock(512, kernel=2, stride=2, padding=0),
 
-            Conv2DBlock(512, 1024, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
-            Conv2DBlock(1024, 512, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
-            Conv2DBlock(512, 1024, kernel=3, stride=1, padding=1, atvn='prlu', bn=True, dropout=True),
-            Conv2DBlock(1024, 512, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=True),
-            Conv2DBlock(512, 1024, kernel=3, stride=1, padding=1, atvn='prlu', bn=True, dropout=True),]
+            Conv2DBlock(512, 1024, kernel=3, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
+            Conv2DBlock(1024, 512, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
+            Conv2DBlock(512, 1024, kernel=3, stride=1, padding=1, atvn='prlu', bn=True, dropout=False),
+            Conv2DBlock(1024, 512, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=False),
+            Conv2DBlock(512, 1024, kernel=3, stride=1, padding=1, atvn='prlu', bn=True, dropout=False),]
         )
         self.encoder = seq1.block
         NN_size = int(seq1.flattend_size)
-        self.classOut = nn.Sequential(Conv2DBlock(1024, 5 * 100, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=True).block)
-        self.bboxOut = nn.Sequential(Conv2DBlock(1024, 5 * 5, kernel=1, stride=1, padding=0, atvn='prlu', bn=True, dropout=True).block)
+        self.classOut = nn.Sequential(Conv2DBlock(1024, 5 * 100, kernel=1, stride=1, padding=0, atvn=None, bn=False, dropout=False).block)
+        self.bboxOut = nn.Sequential(Conv2DBlock(1024, 5 * 5, kernel=1, stride=1, padding=0, atvn=None, bn=True, dropout=True).block)
         self.init_w()
-
-        print(seq1.output_size)
-
-
 
     def init_w(self):
         for m in self.modules():
@@ -66,14 +62,13 @@ class Model_CNN_0(nn.Module):
         classOut = torch.reshape(classOut, (-1, 7, 7, 5, 100))
         bboxOut = self.bboxOut(x)
         bboxOut = torch.reshape(bboxOut, (-1, 7, 7, 5, 5))
-
         return bboxOut, classOut
 
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     m = nn.DataParallel(Model_CNN_0()).to(device)
-    img1 = torch.zeros((5, 3, 448, 448), dtype=torch.float).cuda()
-    x, _ = m.forward(img1)
-    print(x.shape)
+    img1 = torch.zeros((10, 3, 448, 448), dtype=torch.float).cuda()
+    bboxOut, classOUt= m.forward(img1)
+    print(bboxOut.shape)
     # du, dw, dtr, du_cov, dw_cov, dtr_cov = m.forward(img1, img2)
     # print(m)
